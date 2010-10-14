6224a4779cec89a6263d885d0ce1241757da8ce4
Denis Odorcic <denis.odorcic@gmail.com>
2010-10-14 19:14:20 -0400

    Add find_all_across_pages_since to recording

--- lib/highrise/recording.rb
+++ lib/highrise/recording.rb
@@ -1,4 +1,9 @@
 module Highrise
   class Recording < Base
+    include Pagination
+
+    def self.find_all_across_pages_since(time)
+      find_all_across_pages(:params => { :since => time.utc.to_s(:db).gsub(/[^\d]/, '') })
+    end
   end
 end
--- spec/highrise/recording_spec.rb
+++ spec/highrise/recording_spec.rb
@@ -10,5 +10,14 @@ describe Highrise::Recording do
     @recording.kind_of?(Highrise::Base).should be_true
   end
 
+  describe ".find_all_across_pages_since" do
+
+    it "should delegate to find_all_across_pages with correct params" do
+      time = Time.parse("Wed Jan 14 15:43:11 -0200 2009")
+      Highrise::Recording.should_receive(:find_all_across_pages).with({:params=>{:since=>"20090114174311"}}).and_return("result")
+      Highrise::Recording.find_all_across_pages_since(time).should == "result"
+    end
+
+  end
 
 end
